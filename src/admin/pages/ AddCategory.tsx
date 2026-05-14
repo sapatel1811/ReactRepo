f6d53987749239d73f60040c5c8d6b50df312eac
sapatel1811 <sapna.verve@gmail.com>
2026-05-14 12:13:02 +0530

    Update  AddCategory.tsx

--- src/admin/pages/ AddCategory.tsx
+++ src/admin/pages/ AddCategory.tsx
@@ -49,7 +49,7 @@ function AddCategory() {
     if (!validation()) return;
 
     try {
-      const res = await axios.post("https://reactrepo-1l35.onrender.com/addcategories", {
+      const res = await axios.post("http://localhost:3000/addcategories", {
         ...formdata,
         id: Date.now().toString(),
       });
